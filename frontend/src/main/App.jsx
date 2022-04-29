f84cef5d18d05c3a72d524ba353188aa55b82020
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-29 10:23:59 -0300

    Comp. Content

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,14 +5,12 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
-
+import Home from '../components/home/home'
 export default props => 
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon='home' title='Início' 
-            subtitle='Segundo Projeto do capítulo de React.'/>
+        <Home />
         <Footer />
     </div>
